fb132112e6d75f598c382640fb8401eea551759f
Hami Ibriyamov <hamop3108@gmail.com>
2023-07-03 20:15:00 +0300

    Update CartesianProductPairCalculator.cs

--- CartesianProductPairCalculator.cs
+++ CartesianProductPairCalculator.cs
@@ -1,33 +1,44 @@
+using System.Text;
+
 namespace CartesianProductPairCalculator
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            HashSet<int> A = new HashSet<int>()
+            List<int> a = new List<int>()
             {
                 1,2,3,4,5,6,
             };
-            HashSet<int> B = new HashSet<int>()
+            List<int> b = new List<int>()
             {
                 2,3,4,5,6,7,8
             };
 
-            int pairs = PairCalculator(A, B);
-            Console.WriteLine(pairs);
+            (StringBuilder aXb, int pairs) = PairCalculator(a, b);
+
+            Console.WriteLine(string.Join("", aXb));
+
+            Console.WriteLine();
+
+            Console.WriteLine("Total pairs in the Cartesian product: " + pairs);
         }
-        public static int PairCalculator(HashSet<int> A, HashSet<int> B)
+        public static Tuple<StringBuilder, int> PairCalculator(List<int> a, List<int> b)
         {
+            StringBuilder CP = new StringBuilder();
             int pairs = 0;
 
-            for (int i = 0; i < A.Count; i++)
+            for (int i = 0; i < a.Count; i++)
             {
-                for (int r = 0; r < B.Count; r++)
+                for (int r = 0; r < b.Count; r++)
                 {
+                    CP.Append("[" + a[i].ToString() + "," + b[r].ToString() + "]; ");
                     pairs++;
                 }
+                CP.AppendLine();
             }
-            return pairs;
+
+            return Tuple.Create(CP, pairs);
         }
     }
 }
