82312e6b4a67795c8351f69164e8a608b45ad6e7
Hami Ibriyamov <hamop3108@gmail.com>
2024-06-18 16:27:15 +0300

    Create The "this" keyword.cs

---
+++ The "this" keyword.cs
@@ -0,0 +1,26 @@
+namespace This
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ClassA reFa = new ClassA();
+
+        }
+        
+    }
+
+    class ClassA
+    {
+        public ClassA() : this(10) // initializes the consturctor with the given int value
+        {
+            
+        }
+
+        public ClassA(int pValue)
+        {
+
+        }
+    }
+}
