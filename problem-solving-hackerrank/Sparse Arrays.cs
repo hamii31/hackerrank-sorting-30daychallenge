1f43cc8d93504c1519481fc3c6900831629140c7
Hami Ibriyamov <hamop3108@gmail.com>
2024-06-23 09:48:39 +0300

    Create Sparse Arrays.cs

---
+++ problem-solving-hackerrank/Sparse Arrays.cs
@@ -0,0 +1,68 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'matchingStrings' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. STRING_ARRAY stringList
+     *  2. STRING_ARRAY queries
+     */
+
+    public static List<int> matchingStrings(List<string> stringList, List<string> queries)
+    {
+        // Determine how many times a query repeats in the string list, return a list of the results
+
+        List<int> result = new List<int>();
+
+        for (int i = 0; i < queries.Count; i++) {
+            result.Add(stringList.Where(x => x == queries[i]).Count());
+        }
+
+        return result;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int stringListCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<string> stringList = new List<string>();
+
+        for (int i = 0; i < stringListCount; i++)
+        {
+            string stringListItem = Console.ReadLine();
+            stringList.Add(stringListItem);
+        }
+
+        int queriesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<string> queries = new List<string>();
+
+        for (int i = 0; i < queriesCount; i++)
+        {
+            string queriesItem = Console.ReadLine();
+            queries.Add(queriesItem);
+        }
+
+        List<int> res = Result.matchingStrings(stringList, queries);
+    }
+}
